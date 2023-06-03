d9329a48ce0429cbeff2506428e1a1ceff3751a0
kazi031 <kazirafidraiyan031@gmail.com>
2023-06-03 22:57:43 +0600

    Task Manager App

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 
 
-const AddTask = () => {
+const AddTask = ({onAdd}) => {
   const [text, setText] = useState('')
   const [day, setDay] = useState('')
   const [reminder, setReminder] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    
+    if(!text) {
+      alert('Please add a Task')
+      return
+    }
+
+    onAdd({ text, day, reminder})
+
+    setText('')
+    setDay('')
+    setReminder(false)
+  }
+
   return (
-    <form className='add-form'>
+    <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
           <label>Task</label>
           <input type='text' placeholder='Add Task'
@@ -19,7 +35,8 @@ const AddTask = () => {
         </div>
         <div className='form-control form-control-check'>
           <label>Set Reminder</label>
-          <input type='checkbox' 
+          <input type='checkbox'
+          checked = {reminder} 
           value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
         </div>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({title}) => {
-  const onClick = () => {
+const Header = ({title , onAdd, showAdd}) => {
+  {/*const onClick = () => {
     console.log('Click')
-  }
+  }*/}
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button color = 'green' text = 'Hello'
-        onClick = {onClick}/>
+        <Button color =  {showAdd ? 'red' : 'green'}
+        text = {showAdd ? 'Close' : 'Add'}
+        onClick = {onAdd}/>
         {/* These are reusable components 
         <Button color = 'red' text = 'Hello'/>
         <Button color = 'black' text = 'Hello'/> */}
